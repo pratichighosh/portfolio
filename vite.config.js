52a2164c65275a152db6134b0e134b3273b0a086
Pratichi Ghosh <pratichighosh2003@gmail.com>
2025-02-03 00:58:53 +0530

    Updated base URL in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',  // Add this line
+  base: '/portfolio/', // Changed to match your GitHub repo name
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  }
 })
